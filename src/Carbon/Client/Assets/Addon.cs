9a84dd02350384fc881b0a0318196b6ee0f8441c
raul <raulssorban@gmail.com>
2023-09-04 06:14:02 +0200

    Addon importing (buffer, file)

--- src/Carbon/Client/Assets/Addon.cs
+++ src/Carbon/Client/Assets/Addon.cs
@@ -28,13 +28,25 @@ public class Addon : IStore<Addon, Asset>
 		};
 	}
 
+	public static Addon ImportFromBuffer(byte[] buffer)
+	{
+		return Serializer.Deserialize<Addon>(new ReadOnlySpan<byte>(buffer, 0, buffer.Length));
+
+	}
+	public static Addon ImportFromFile(string path)
+	{
+		var data = OsEx.File.ReadBytes(path);
+		var result = ImportFromBuffer(data);
+		Array.Clear(data, 0, data.Length);
+		data = null;
+		return result;
+	}
 	public byte[] Store()
 	{
 		using var stream = new MemoryStream();
 		Serializer.Serialize(stream, this);
 		return stream.ToArray();
 	}
-
 	public void StoreToFile(string path)
 	{
 		path += EXTENSION;
