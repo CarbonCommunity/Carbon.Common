ed934c2d5edcf5116eb90bd36c22989065fd4e80
raul <raulssorban@gmail.com>
2023-09-14 05:30:02 +0200

    Baseline

---
+++ src/Carbon/Client/NoMap.cs
@@ -0,0 +1,34 @@
+﻿/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community 
+ * All rights reserved.
+ *
+ */
+
+namespace Carbon.Client;
+
+public struct NoMap
+{
+	public const string MAP_URL = "https://carbonmod.gg/assets/content/blank.map";
+
+	public static bool Enabled => CommandLineEx.GetArgumentExists("+nomap");
+
+	public static void Init()
+	{
+		if (!Enabled)
+		{
+			return;
+		}
+
+		ConVar.Server.levelurl = MAP_URL;
+	}
+	public static void TerrainPostprocess()
+	{
+		if (!Enabled)
+		{
+			return;
+		}
+
+		TerrainMeta.Collider.enabled = false;
+	}
+}
