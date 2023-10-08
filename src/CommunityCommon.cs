fc2e7ad5f767b705924a7ceaa0851593f5534597
raul <raulssorban@gmail.com>
2023-10-08 16:26:40 +0200

    Added modules loaded/total and extension count to console info (Windows-only)

--- src/CommunityCommon.cs
+++ src/CommunityCommon.cs
@@ -13,7 +13,7 @@ using Carbon.Client.SDK;
 
 /*
  *
- * Copyright (c) 2022-2023 Carbon Community 
+ * Copyright (c) 2022-2023 Carbon Community
  * All rights reserved.
  *
  */
@@ -282,7 +282,7 @@ public class Community
 #if MINIMAL
 			$" Minimal" +
 #endif
-			$" v{version}, {ModLoader.LoadedPackages.Count:n0} mods, {ModLoader.LoadedPackages.Sum(x => x.Plugins.Count):n0} plgs";
+			$" v{version}, {ModLoader.LoadedPackages.Count:n0} mods, {ModLoader.LoadedPackages.Sum(x => x.Plugins.Count):n0} plgs, {ModuleProcessor.Modules.Count(x => x is BaseModule module && module.GetEnabled()):n0}/{ModuleProcessor.Modules.Count:n0} mdls, {AssemblyEx.Extensions.Loaded.Count:n0} exts";
 #endif
 	}
 
