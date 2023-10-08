120884deb5de84a106b2d0f167ec43b5f3702672
raul <raulssorban@gmail.com>
2023-10-08 23:31:58 +0200

    Only install the `c.scriptdebugorigin` command on Debug builds (config remains) Fixed auth level

--- src/Carbon/Core/Core.Commands.Debugging.cs
+++ src/Carbon/Core/Core.Commands.Debugging.cs
@@ -9,6 +9,9 @@ namespace Carbon.Core;
 
 public partial class CorePlugin : CarbonPlugin
 {
+#if DEBUG
 	[CommandVar("scriptdebugorigin", "[For debugging purposes] Overrides the script directory to this value so remote debugging is possible.")]
+	[AuthLevel(2)]
 	private string ScriptDebuggingOrigin { get { return Community.Runtime.Config.ScriptDebuggingOrigin; } set { Community.Runtime.Config.ScriptDebuggingOrigin = value; } }
+#endif
 }
