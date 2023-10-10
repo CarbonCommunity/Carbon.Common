71dac849cb81ea6ed6ace7a33c6b488e3e9fe04c
raul <raulssorban@gmail.com>
2023-10-10 12:31:49 +0200

    IZipScriptProcessor is impostor

--- src/Contracts/Carbon/IZipScriptProcessor.cs
+++ src/Contracts/Carbon/IZipScriptProcessor.cs
@@ -1,20 +1,7 @@
 ﻿namespace Carbon.Contracts;
 
-public interface IZipScriptProcessor : IBaseProcessor, IDisposable
+public interface IZipScriptProcessor : IScriptProcessor, IDisposable
 {
-	void InvokeRepeating(Action action, float delay, float repeat);
-
-	GameObject gameObject { get; }
-
-	bool AllPendingScriptsComplete();
-	bool AllNonRequiresScriptsComplete();
-	bool AllExtensionsComplete();
-
-	void StartCoroutine(IEnumerator coroutine);
-	void StopCoroutine(IEnumerator coroutine);
-
-	void Remove(string name);
-
 	public interface IZipScript : IInstance
 	{
 		IScriptLoader Loader { get; set; }
