ebbf5feb1b5e7287032afe12fdf0542674ddb8e0
raul <raulssorban@gmail.com>
2023-12-23 17:52:38 +0100

    Core.Hooks code gen

---
+++ src/Carbon/Core/Core.Hooks.Client.cs
@@ -0,0 +1,20 @@
+﻿/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community
+ * All rights reserved.
+ *
+ */
+
+using Carbon.Client.SDK;
+using Connection = Network.Connection;
+
+namespace Carbon.Core;
+#pragma warning disable IDE0051
+
+public partial class CorePlugin : CarbonPlugin
+{
+	private void IOnCarbonClientReady(ICarbonClient client)
+	{
+		Community.Runtime.CarbonClientManager.SendRequestToPlayer(client.Connection);
+	}
+}
