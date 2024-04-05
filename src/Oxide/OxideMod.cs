818e1ecc80f5d1bae1f86d49efff9af65b23682d
raul <raulssorban@gmail.com>
2024-04-05 12:25:23 +0200

    Fixed `OxideMod.UnloadPlugin`

--- src/Oxide/OxideMod.cs
+++ src/Oxide/OxideMod.cs
@@ -113,10 +113,15 @@ public class OxideMod
 
 	public bool UnloadPlugin(string name)
 	{
-		var isValid = Community.Runtime.ScriptProcessor.InstanceBuffer.ContainsKey(name);
-		Community.Runtime.ScriptProcessor.Remove(name);
+		if (Community.Runtime.ScriptProcessor.InstanceBuffer.TryGetValue(name, out var instance))
+		{
+			instance?.Clear();
+			instance?.Dispose();
+			Community.Runtime.ScriptProcessor.Remove(name);
+			return true;
+		}
 
-		return isValid;
+		return true;
 	}
 
 	public void OnSave()
