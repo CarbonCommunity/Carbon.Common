5c79d6d7107fa694a3f60cf76cd3dc882136fa39
raul <raulssorban@gmail.com>
2024-05-24 15:57:50 +0200

    Added `AddComponentCache<>`, `GetComponentCache<>` and `RemoveComponentCache<>` which handles GetComponent calls for you in a cached component bank with the purpose of faster component lookups This is something a few plugins but not most do already, standardizing it is a good idea for the long run

---
+++ src/Carbon/Extensions/UnityEx.cs
@@ -0,0 +1,98 @@
+﻿/*
+ *
+ * Copyright (c) 2022-2024 Carbon Community
+ * All rights reserved.
+ *
+ */
+
+using Facepunch;
+
+namespace Carbon.Extensions;
+
+public static class UnityEx
+{
+	public static ComponentCacheBank ComponentCache = new();
+
+	public static T AddComponentCache<T>(this GameObject go) where T : MonoBehaviour
+	{
+		if (go == null)
+		{
+			return default;
+		}
+
+		return ComponentCache.Add<T>(go);
+	}
+
+	public static T GetComponentCache<T>(this GameObject go) where T : MonoBehaviour
+	{
+		if (go == null)
+		{
+			return default;
+		}
+
+		return ComponentCache.Get<T>(go);
+	}
+
+	public static bool RemoveComponentCache<T>(this GameObject go) where T : MonoBehaviour
+	{
+		if (go == null)
+		{
+			return default;
+		}
+
+		return ComponentCache.Remove<T>(go);
+	}
+
+	public class ComponentCacheBank : Dictionary<GameObject, List<MonoBehaviour>>
+	{
+		public T Add<T>(GameObject go) where T : MonoBehaviour
+		{
+			if (!this.TryGetValue(go, out var monos))
+			{
+				this[go] = monos = new();
+			}
+
+			var mono = go.AddComponent<T>();
+
+			monos.Add(mono);
+
+			return mono;
+		}
+
+		public T Get<T>(GameObject go) where T : MonoBehaviour
+		{
+			if (!this.TryGetValue(go, out var monos))
+			{
+				this[go] = monos = new();
+			}
+
+			var existent = monos.FirstOrDefault(x => x is T);
+
+			if (existent == null)
+			{
+				monos.Add(existent = go.GetComponent<T>());
+			}
+
+			return existent as T;
+		}
+
+		public bool Remove<T>(GameObject go) where T : MonoBehaviour
+		{
+			if (!this.TryGetValue(go, out var monos))
+			{
+				this[go] = monos = new();
+			}
+
+			return monos.RemoveAll(x =>
+			{
+				if (x is T)
+				{
+					GameObject.DestroyImmediate(x);
+					return true;
+				}
+
+				return false;
+			}) > 0;
+		}
+	}
+}
