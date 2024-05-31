4030234c143ef565d64a71dc93087e0a4e4dec16
raul <raulssorban@gmail.com>
2024-05-31 12:47:25 +0200

    Cleanup and finalized component cache revamp

--- src/Carbon/Components/ComponentCacheBank.cs
+++ src/Carbon/Components/ComponentCacheBank.cs
@@ -1,52 +1,11 @@
 ﻿/*
  *
- * Copyright (c) 2022-2024 Carbon Community  
+ * Copyright (c) 2022-2024 Carbon Community
  * All rights reserved.
  *
  */
 
 namespace Carbon.Components;
-
-public static class ComponentCacheBankNonGeneric
-{
-	public static List<IComponentBank> All = new();
-
-	public static T AddComponentCache<T>(this GameObject go) where T : MonoBehaviour
-	{
-		return go == null ? default : ComponentCacheBank<T>.Instance.Add(go);
-	}
-	public static T GetComponentCache<T>(this GameObject go) where T : MonoBehaviour
-	{
-		return go == null ? default : ComponentCacheBank<T>.Instance.Get(go);
-	}
-	public static bool RemoveComponentCache<T>(this GameObject go) where T : MonoBehaviour
-	{
-		return go == null ? default : ComponentCacheBank<T>.Instance.Remove(go);
-	}
-	public static bool TryGetOrAddComponentCache<T>(this GameObject go, out T component) where T : MonoBehaviour
-	{
-		return (component = go.GetComponentCache<T>() ?? go.AddComponentCache<T>()) != null;
-	}
-	public static bool DestroyCache(this GameObject go)
-	{
-		if (go == null)
-		{
-			return false;
-		}
-
-		return All.Count(cache => cache.Destroy(go)) > 0;
-	}
-
-	internal static void InternalEntityDestroy(BaseEntity entity)
-	{
-		if (entity == null || entity.gameObject == null)
-		{
-			return;
-		}
-
-		All.ForEach(cache => cache.Destroy(entity.gameObject));
-	}
-}
 
 public class ComponentCacheBank<T> : Dictionary<GameObject, List<T>>, IComponentBank where T : MonoBehaviour
 {
