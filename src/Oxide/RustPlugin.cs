c32e60fb7a9f7a47c1d86deb29d5763306f3711e
raul <raulssorban@gmail.com>
2024-06-02 06:30:39 +0200

    Phrases

--- src/Oxide/RustPlugin.cs
+++ src/Oxide/RustPlugin.cs
@@ -257,7 +257,7 @@ public class RustPlugin : Plugin
 	}
 	public void ILoadDefaultMessages()
 	{
-		CallHook("LoadDefaultMessages");
+		LoadDefaultMessages();
 	}
 
 	public override string ToPrettyString()
