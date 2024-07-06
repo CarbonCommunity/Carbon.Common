3c273bf12f55f366f6c07b9760618f1d8a438299
raul <raulssorban@gmail.com>
2024-07-06 20:35:00 +0200

    Added `IOnCupboardAuthorize`

---
+++ src/Carbon/Core/Core.Hooks.Structure.cs
@@ -0,0 +1,29 @@
+﻿/*
+ *
+ * Copyright (c) 2024 Carbon Community
+ * All rights reserved.
+ *
+ */
+
+namespace Carbon.Core;
+#pragma warning disable IDE0051
+
+public partial class CorePlugin : CarbonPlugin
+{
+	private object IOnCupboardAuthorize(ulong userID, BasePlayer player, BuildingPrivlidge privlidge)
+	{
+		if (userID == player.userID)
+		{
+			if (HookCaller.CallStaticHook(1460091328, privlidge, player) != null)
+			{
+				return true;
+			}
+		}
+		else if (HookCaller.CallStaticHook(2217887722, privlidge, userID, player) != null)
+		{
+			return true;
+		}
+
+		return null;
+	}
+}
