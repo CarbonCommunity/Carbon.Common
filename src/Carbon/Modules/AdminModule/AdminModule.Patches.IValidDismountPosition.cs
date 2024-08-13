e3c89f4765b04ea50ce5af12df0048fdaf2661dc
raul <raulssorban@gmail.com>
2024-08-13 23:53:54 +0200

    Cleanup

--- src/Carbon/Modules/AdminModule/AdminModule.Patches.IValidDismountPosition.cs
+++ src/Carbon/Modules/AdminModule/AdminModule.Patches.IValidDismountPosition.cs
@@ -7,10 +7,9 @@ namespace Carbon.Modules;
 public partial class AdminModule
 {
 	[HookAttribute.Patch("IValidDismountPosition", "IValidDismountPosition", typeof(BaseMountable), "ValidDismountPosition", new System.Type[] { typeof(BasePlayer), typeof(Vector3) })]
-	[HookAttribute.Identifier("cb1bb2048c2946d8ad469bc218193a7a")]
 	[HookAttribute.Options(HookFlags.Hidden)]
 
-	public class BaseMountable_ValidDismountPosition_cb1bb2048c2946d8ad469bc218193a7a : API.Hooks.Patch
+	public class BaseMountable_ValidDismountPosition : API.Hooks.Patch
 	{
 		public static bool Prefix(BasePlayer player, Vector3 disPos, BaseMountable __instance, ref bool __result)
 		{
