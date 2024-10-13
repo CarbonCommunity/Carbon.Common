560dc4d4ecdba4a0563b7d0c5ea1fa22329c3c12
raul <raulssorban@gmail.com>
2024-10-13 20:56:36 +0200

    Added C4C protocol

---
+++ src/Carbon/Client/Protocol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbon.Client;
+
+public class Protocol
+{
+	public const int VERSION = 100;
+}
