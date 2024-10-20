9098068c0126fa74082f860c8017f86389c72745
raul <raulssorban@gmail.com>
2024-10-20 20:16:26 +0200

    Reintroduced C4C editor RCON command Client hooks

---
+++ src/Carbon/Core/Core.Hooks.Client.cs
@@ -0,0 +1,40 @@
+﻿using Carbon.Client.SDK;
+
+namespace Carbon.Core;
+
+#pragma warning disable IDE0051
+
+public partial class CorePlugin
+{
+	private void IOnCarbonClientReady(ICarbonConnection client)
+	{
+		if (!Community.Runtime.ClientConfig.Enabled)
+		{
+			return;
+		}
+
+		Logger.Log($"{client.Username}[{client.UserId}] is ready");
+	}
+
+	private void OnClientAddonsDownload(ICarbonConnection client)
+	{
+		if (!Community.Runtime.ClientConfig.Enabled)
+		{
+			return;
+		}
+
+		Logger.Log($"{client.Username}[{client.UserId}] is downloading addons");
+		client.IsDownloadingAddons = true;
+	}
+
+	private void OnClientAddonsFinalized(ICarbonConnection client)
+	{
+		if (!Community.Runtime.ClientConfig.Enabled)
+		{
+			return;
+		}
+
+		Logger.Log($"{client.Username}[{client.UserId}] finished downloading addons");
+		client.IsDownloadingAddons = false;
+	}
+}
