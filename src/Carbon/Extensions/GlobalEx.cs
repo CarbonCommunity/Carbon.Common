a9a37d4abc5e58c1009463e8026a309b36ee4f69
raul <raulssorban@gmail.com>
2025-11-26 17:53:11 +0100

    Added `CancelAllInvokes` global extension (so you can do `entity.CancelAllInvokes()`) which turns off all entity invokes active on the entity/FacepunchBehaviour

--- src/Carbon/Extensions/GlobalEx.cs
+++ src/Carbon/Extensions/GlobalEx.cs
@@ -1,4 +1,6 @@
-﻿public static class GlobalEx
+﻿using Facepunch;
+
+public static class GlobalEx
 {
 	public static bool IsSteamId(this string id)
 	{
@@ -14,5 +16,20 @@
 	public static void Clear(this Effect effect, bool _)
 	{
 		effect.Clear();
+	}
+
+	public static void CancelAllInvokes(this FacepunchBehaviour behaviour)
+	{
+		using var invokes = Pool.Get<PooledList<InvokeAction>>();
+		InvokeHandler.FindInvokes(behaviour, invokes);
+		for (int i = 0; i < invokes.Count; i++)
+		{
+			var invoke = invokes[i];
+			if (invoke.action == null)
+			{
+				continue;
+			}
+			behaviour.CancelInvoke(invoke.action);
+		}
 	}
 }
