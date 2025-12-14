24e663d791afc6fe883f7fbb2d9c82f02d070463
evs.ptr <evs.ptr@proton.me>
2025-12-14 15:09:00 +0500

    `ImageDatabaseModule` propagate `Success` & `CRC` for `ImageQueueResult`

--- src/Carbon/Modules/ImageDatabaseModule/ImageDatabaseModule.cs
+++ src/Carbon/Modules/ImageDatabaseModule/ImageDatabaseModule.cs
@@ -310,8 +310,9 @@ public partial class ImageDatabaseModule : CarbonModule<ImageDatabaseConfig, Emp
 			{
 				if (results != null)
 				{
-					foreach (var result in results)
+					for (var index = 0; index < results.Count; index++)
 					{
+						var result = results[index];
 						if (result.Data.Length >= MaximumBytes)
 						{
 							Puts($"Failed storing {urlCount:n0} jobs: {result.Data.Length} more or equal than {MaximumBytes}");
@@ -323,6 +324,9 @@ public partial class ImageDatabaseModule : CarbonModule<ImageDatabaseConfig, Emp
 						if (id != 0)
 						{
 							_protoData.Map[GetId(result.Url)] = id;
+							result.Success = true;
+							result.CRC = id;
+							results[index] = result;
 						}
 					}
 
