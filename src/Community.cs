b302ea486e8faad158e41a8569bc7d62bbeffbcf
raul <raulssorban@gmail.com>
2025-12-16 21:50:34 +0100

    Fixed NRE in RefreshConsoleInfo when `-noconsole` is present

--- src/Community.cs
+++ src/Community.cs
@@ -77,9 +77,14 @@ public partial class Community
 	public void RefreshConsoleInfo()
 	{
 #if WIN
-		if (!IsConfigReady || !Config.Misc.ShowConsoleInfo) return;
-
-		if (!IsServerInitialized || ServerConsole.Instance == null) return;
+		if (!IsConfigReady || !Config.Misc.ShowConsoleInfo)
+		{
+			return;
+		}
+		if (!IsServerInitialized || ServerConsole.Instance == null || ServerConsole.Instance.input == null)
+		{
+			return;
+		}
 		if (ServerConsole.Instance.input.statusText.Length != 4) ServerConsole.Instance.input.statusText = new string[4];
 
 		var version =
