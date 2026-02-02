ca6b592b43ed740d8dccfc67bdc785c62e7d835d
raul <raulssorban@gmail.com>
2026-02-02 12:51:21 +0100

    No longer reconstruct the moved entity if it's been moved in the same network group it started being moved in Only do so when groups change - fixes model going invisible while still collidable

--- src/Carbon/Modules/HammerModule/HammerModule.cs
+++ src/Carbon/Modules/HammerModule/HammerModule.cs
@@ -632,6 +632,7 @@ public partial class HammerModule : CarbonModule<HammerModule.HammerConfig, Empt
 		RaycastHit hit = default;
 		entity?.SetParent(null, true);
 		var transform = entity?.transform;
+		var group = entity?.net.group;
 		while (player.IsValid() && entity.IsValid() && entityMovingPlayers.Contains(player.userID))
 		{
 			hits.Clear();
@@ -709,7 +710,7 @@ public partial class HammerModule : CarbonModule<HammerModule.HammerConfig, Empt
 		}
 		Pool.FreeUnmanaged(ref hits);
 
-		if (entity is not BaseCorpse && !entity.HasEntityInParents(player) && !player.HasEntityInParents(entity))
+		if (entity.IsValid() && entity is not BaseCorpse && !entity.HasEntityInParents(player) && !player.HasEntityInParents(entity) && entity.net.group != group)
 		{
 			ReconstructEntity(entity);
 		}
