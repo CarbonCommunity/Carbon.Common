348669b8027199fc9ed53d17fbb28fbad6180cb2
raul <raulssorban@gmail.com>
2026-02-05 12:06:52 +0100

    Allow chairs to be moved with the Hammer UI

--- src/Carbon/Modules/HammerModule/HammerModule.cs
+++ src/Carbon/Modules/HammerModule/HammerModule.cs
@@ -142,6 +142,7 @@ public partial class HammerModule : CarbonModule<HammerModule.HammerConfig, Hamm
 			Barricade => false,
 
 			HotAirBalloon => true,
+			BaseChair => true,
 			BaseBoat => true,
 			BaseCorpse => true,
 			BaseLadder => true,
